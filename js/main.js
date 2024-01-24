60780af93775d4739a688b0807a2072c77065c79
Ziyodulla <admin@jbnuu.uz>
2024-01-24 19:50:09 +0500

    LastChanges

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const todoAddForm = document.querySelector('.todo-add-form');
 const todoListArea = document.querySelector('.todo-lists');
 const todoItemsSortBtns = document.querySelectorAll('.todoItemsSortBtns');
+const todoLeftItems = document.querySelector('.todoLeftItems');
 const todoItems = JSON.parse(localStorage.getItem('todoItems')) || [];
 
 
@@ -22,6 +23,7 @@ function showTodoItems(items,todoListArea){
   todoListArea.innerHTML = items.map((item,index) => {
     return `<li data-id = '${item.id}' class='${(item.status == true) ? 'checked' : ''}'>${item.text}</li>`;
   }).join('');
+  todoLeftItemsCount();
 }
 
 function changeItemStatus(event){
@@ -34,12 +36,30 @@ function changeItemStatus(event){
 
     return item;
   });
+  todoLeftItemsCount();
   saveToLocalStorage(newToDoItems);
 }
 
 function todoItemsSort(event){
     const sortType = event.target.dataset.type;
-    console.log(sortType);
+    switch(sortType){
+      case 'all' : 
+      showTodoItems(todoItems,todoListArea);
+      break;
+      case 'active':
+        const todoActiveItems = todoItems.filter((items) => items.status == false);
+        showTodoItems(todoActiveItems,todoListArea);
+      break;
+      case 'completed':
+        const todoCompletedItems = todoItems.filter((items) => items.status == true);
+        showTodoItems(todoCompletedItems,todoListArea);
+      break;
+    }
+}
+
+function todoLeftItemsCount(){
+  const todoLeftItemsCount = todoItems.filter((items) => items.status == true);
+  todoLeftItems.innerHTML = `<span>${todoLeftItemsCount.length}</span> items left`;
 }
 
 function saveToLocalStorage(items){
